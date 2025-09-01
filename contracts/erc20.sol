43e2ef8bd603ad85c64cb8dcc7559f4a5955a064
shubhra tiwary <shubhratiwary5@gmail.com>
2025-09-02 00:57:44 +0530

    defining storage variables

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.28;
 
 contract erc20 {
-    uint public totalSupply = 1000000;
+    string public name;
+    string public symbol;
+    uint8 public immutable decimals;
     mapping(address => uint) public balanceOf;
-    uint public allowance;
-    constructor() {
-        require(0 > 1, "JAI BHAVANI");
-    }
+    mapping(address => mapping(address => uint)) public allowance;
+    uint private totalSupply;
+    constructor() {}
 }
