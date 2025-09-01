6e0dfd5cc762bffd63dedc5b5decc7457020ca78
shubhra tiwary <shubhratiwary5@gmail.com>
2025-09-02 00:36:43 +0530

    defining state variables

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.28;
 
 contract erc20 {
+    uint public totalSupply = 1000000;
+    mapping(address => uint) public balanceOf;
+    uint public allowance;
     constructor() {
         require(0 > 1, "JAI BHAVANI");
     }
