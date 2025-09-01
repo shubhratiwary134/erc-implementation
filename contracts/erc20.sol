1f24b134e67b2e08a88a0a6cd5b297d04f95a62d
shubhra tiwary <shubhratiwary5@gmail.com>
2025-09-02 02:32:09 +0530

    balanceOf function by the erc-20 standard .

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -5,12 +5,15 @@ contract erc20 {
     string private name;
     string private symbol;
     uint8 private immutable decimals;
-    mapping(address => uint) private balanceOf;
+    mapping(address => uint) private balances;
     mapping(address => mapping(address => uint)) private allowance;
     uint private totalSupply;
     constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
+    }
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
     }
 }
