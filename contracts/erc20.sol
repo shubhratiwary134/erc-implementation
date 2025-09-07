a2aec01b80a7ad45d1a65a50c351100754b9d56b
shubhra tiwary <shubhratiwary5@gmail.com>
2025-09-07 16:37:44 +0530

    allowance , minting and burning logic implemented .

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -10,6 +10,7 @@ contract erc20 {
     uint private _totalSupply;
 
     event Transfer(address indexed from, address indexed to, uint value);
+    event Approve(address indexed owner, address indexed spender, uint amount);
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
@@ -62,5 +63,50 @@ contract erc20 {
     function transfer(address to, uint amount) external returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        require(
+            spender != address(0),
+            "Approve to the zero address is not allowed"
+        );
+        _allowance[msg.sender][spender] = amount;
+        emit Approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint amount
+    ) external returns (bool) {
+        require(
+            _allowance[from][msg.sender] >= amount,
+            "Not Authorized for that much amount"
+        );
+        _transfer(from, to, amount);
+        _allowance[from][msg.sender] -= amount;
+        return true;
+    }
+
+    function _mint(address to, uint amount) internal {
+        require(to != address(0), "minting to address 0 not allowed");
+        _totalSupply += amount;
+        balances[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint amount) internal {
+        require(
+            from != address(0),
+            "Burning from the address 0 is not allowed "
+        );
+        require(
+            balances[from] >= amount,
+            "Cant burn more than the amount itself"
+        );
+        balances[from] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
     }
 }
